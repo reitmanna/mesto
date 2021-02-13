ba303452c0468c660734d0a9c2f353c7858e2e22
Anna Reitman <areitmanm@gmail.com>
2021-02-13 15:16:23 +0300

    main

--- js/script.js
+++ js/script.js
@@ -4,27 +4,27 @@ let editButton = profile.querySelector('.button_edit');
 let popup = main.querySelector('.popup');
 let exitButton = popup.querySelector('.button_exit');
 let form = popup.querySelector('.popup__field');
-let newName = form.querySelector('.popup__form_name');
-let newJob = form.querySelector('.popup__form_job');
+let newName = form.querySelector('.popup_form_name');
+let newJob = form.querySelector('.popup_form_job');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
 
 function togglePopup () {
+    popup.classList.toggle('popup_opened');
     if (popup.classList.contains('popup_opened')) {
-        popup.classList.toggle('popup_opened') 
-    }
-    else {
-        popup.classList.toggle('popup_opened');
+        newName.value = profileName.innerHTML;
+        newJob.value = profileJob.innerHTML;
     }
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    profileName.textContent = newName.value;;
+    profileName.textContent = newName.value;
     profileJob.textContent = newJob.value;
-    exitButton.addEventListener('click', togglePopup);
+    togglePopup()
 }
 
 form.addEventListener('submit', formSubmitHandler); 
 
+exitButton.addEventListener('click', togglePopup);
 editButton.addEventListener('click', togglePopup); 
