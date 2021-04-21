4f0714fcac0d25f90e14226536e52d7b12267647
Anna Reitman <areitmanm@gmail.com>
2021-04-22 00:09:36 +0400

    main

--- js/index.js
+++ js/index.js
@@ -57,66 +57,6 @@ function closeWithOverlay(evt) {
 };
 
 
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  errorElement.classList.remove('popup__input-error_active');
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__form'));
-  const buttonElement = formElement.querySelector('.popup__submit');
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-function enableValidation() {
-  const formList = Array.from(document.querySelectorAll('.popup__field'));
-  formList.forEach((formElement) => {
-    profileForm.addEventListener('submit', submitProfileForm);
-    cardForm.addEventListener('submit', submitCardForm);
-    setEventListeners(formElement);
-  });
-}
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
-
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.disabled = true;
-    buttonElement.classList.add('popup__submit_inactive');
-  } else {
-    buttonElement.disabled = false;
-    buttonElement.classList.remove('popup__submit_inactive');
-  }
-}
-
-enableValidation();
-
-
 
 function profileAddForm() {
   newName.value = profileName.textContent;
